abe72e42d742d3218749d2c9ff5b2bfb8534eb7a
DESKTOP-B16ISL6\Joyce <joyceljy6256@gmail.com>
2017-09-20 21:12:05 +0800

    Create

---
+++ farmer.js
@@ -0,0 +1,161 @@
+var answerArr = [];
+var resultArr = [];
+//生命起始值
+var life = 3;
+//農田總數
+var groundnum = 6;
+//生命值圖片路徑
+var lifeImg = "pic/heart.png";
+init();
+displayLife(life);
+//初始化
+function init() {
+    //顯示倒數畫面
+    document.getElementById("div_precountdown").className =
+        document.getElementById("div_precountdown").className.replace(/\bfadeOut\b/, '');
+
+    var imageele = document.getElementsByClassName('precountdown');
+    var image = 'pic/countdown.gif' + '?' + (new Date().getTime());
+    imageele[0].style.background = 'url(' + image + ') center no-repeat';
+
+    //清空陣列
+    answerArr = [];
+    resultArr = [];
+    //產生resultArr
+    generateRange(groundnum, 1, groundnum);
+    //倒數畫面消失
+    setTimeout(function () { fadeOutPrecount(div_precountdown); }, 4000);
+}
+
+//顯示生命值
+function displayLife(life) {
+    var element = document.getElementById('life');
+
+    for (i = 1; i <= life; i++) {
+        var img = document.createElement('img');
+        img.src = lifeImg;
+        img.height = 25;
+        img.width = 25;
+        element.appendChild(img);
+    }
+
+}
+
+//產生亂數陣列
+function generateRange(pCount, pMin, pMax) {
+    min = pMin;
+    max = pMax;
+    //var resultArr = [], randNumber;
+    while (pCount > 0) {
+        randNumber = Math.round(min + Math.random() * (max - min));
+        if (resultArr.indexOf(randNumber) == -1) {
+            resultArr.push(randNumber);
+            pCount--;
+        }
+    }
+    console.log(resultArr)
+    return resultArr;
+}
+//農作物生長
+function grow() {
+
+    var counter = 0;
+    var i = setInterval(function () {
+        hide("ground" + resultArr[counter]);
+        showgrow("grow" + resultArr[counter]);
+
+        counter++;
+        if (counter === resultArr.length) {
+            clearInterval(i);
+        }
+    }, 2000);
+
+}
+//採收
+function harvest(i) {
+    if (answerArr.length < resultArr.length) {
+        answerArr.push(i);
+        console.log(answerArr);
+        if (answerArr.length == resultArr.length) {
+            compareArray(resultArr, answerArr);
+        }
+    }
+};
+
+//比對陣列
+function compareArray(resultArr, answerArr) {
+    var result = _.isEqual(resultArr, answerArr);
+    if (result == false) {
+        answerWrong();
+
+    } else if (result == true) {
+        answerCorrect();
+    }
+    console.log(result);
+}
+
+//隱藏div區塊
+function hide(id) {
+    var element = document.getElementById(id);
+    element.style.display = 'none';
+}
+
+//顯示div區塊
+function showgrow(id) {
+    var element = document.getElementById(id);
+    element.style.display = 'block';
+    element.src = "pic/blossom2.gif" + '?' + (new Date().getTime())
+}
+
+function fadeOutPrecount(id) {
+    var element = document.getElementById(id.id);
+    if (element) {
+        element.className += element.className ? ' fadeOut' : ' fadeOut';
+
+        grow();
+    }
+}
+
+//遊戲結束
+function gameover() {
+    var element = document.getElementById(id);
+    element.style.visibility = 'visible';
+}
+
+//答錯
+function answerWrong() {
+    life--;
+    var element = document.getElementById('life');
+    var image = element.querySelectorAll('[src="' + lifeImg + '"]');
+    var wrongclass = document.getElementsByClassName('wrong');
+    wrongclass[0].style.visibility = 'visible';
+    setTimeout(function() {
+        wrongclass[0].style.visibility = 'hidden';
+        if (image != []) {
+            element.removeChild(image[0]);
+        }
+        if (life == 0) {
+            //遊戲結束
+            gameover();
+        } else {
+            //這關再來一次
+            init();
+        }
+      }, 2000);
+}
+
+//答對
+function answerCorrect(){
+    var correctclass = document.getElementsByClassName('correct');
+    correctclass[0].style.visibility = 'visible';
+    setTimeout(function() {
+        correctclass[0].style.visibility = 'hidden';
+        //進入下一關
+
+      }, 2000);
+}
+
+
+
+
+
