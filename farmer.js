26e2bb09cab078de6c1ba3935088f712c3d71240
DESKTOP-B16ISL6\Joyce <joyceljy6256@gmail.com>
2017-10-06 01:00:53 +0800

    1005

--- farmer.js
+++ farmer.js
@@ -7,7 +7,7 @@ var groundnum = 3;
 //生命值圖片路徑
 var lifeImg = "pic/heart.png";
 //計時4分鐘遊戲結束
-game=setTimeout(gameover, 240000);
+//game = setTimeout(gameover, 240000);
 
 //開始遊戲
 function start() {
@@ -61,13 +61,23 @@ function displayLife(life) {
 
 //關卡設定
 function levelSettings() {
+    //顯示ground
     for (i = 1; i <= resultArr.length; i++) {
         var grd = ("ground" + i).toString();
         var grdele = document.getElementById(grd);
         grdele.style.display = 'block';
+
+        //隱藏growgif
         var grow = ("grow" + i).toString();
         var growele = document.getElementById(grow);
-        growele.getElementsByTagName('img')[0].style.display = 'none';
+        growele.style.display = 'none';
+
+        //隱藏harvestgif
+        var harvest = ("harvest" + i).toString();
+        var harvestele = document.getElementById(harvest);
+        harvestele.classList.remove("changeZindexto0");
+        harvestele.style.display = 'none';
+
     }
     for (i = 1; i <= groundnum; i++) {
         var id = ("ground" + i).toString();
@@ -101,7 +111,7 @@ function grow() {
 
     var counter = 0;
     var i = setInterval(function () {
-        hide("ground" + resultArr[counter]);
+        hideground("ground" + resultArr[counter]);
         showgrow("grow" + resultArr[counter]);
 
         counter++;
@@ -116,6 +126,11 @@ function harvest(i) {
     if (answerArr.length < resultArr.length) {
         answerArr.push(i);
         console.log(answerArr);
+
+        hidegrow("grow" + i);
+        showharvest("harvest" + i);
+
+        //如過採收完畢 則比較陣列
         if (answerArr.length == resultArr.length) {
             compareArray(resultArr, answerArr);
         }
@@ -134,19 +149,19 @@ function compareArray(resultArr, answerArr) {
     console.log(result);
 }
 
-//隱藏div區塊
-function hide(id) {
+//隱藏ground div區塊
+function hideground(id) {
     var element = document.getElementById(id);
 
     element.style.display = 'none';
 }
 
-//顯示div區塊
+//顯示grow div區塊
 function showgrow(id) {
     var element = document.getElementById(id);
     var imgele = element.getElementsByTagName('img')[0];
 
-    imgele.style.display = 'unset';
+    element.style.display = 'unset';
     //imgele.style.vertical-align='top';
     if (id == "grow7" || id == "grow8" || id == "grow9") {
         imgele.src = "pic/carrot_left.gif" + '?' + (new Date().getTime());
@@ -155,6 +170,37 @@ function showgrow(id) {
     } else if (id == "grow4" || id == "grow5" || id == "grow6") {
         imgele.src = "pic/carrot_right.gif" + '?' + (new Date().getTime());
     }
+}
+
+//隱藏grow div區塊
+function hidegrow(id) {
+    var element = document.getElementById(id);
+    element.style.display = 'none';
+}
+
+//顯示harvest div區塊
+function showharvest(id) {
+    var element = document.getElementById(id);
+    var imgele = element.getElementsByTagName('img')[0];
+
+    element.style.display = 'unset';
+    if (id == "harvest7" || id == "harvest8" || id == "harvest9") {
+        assignindex(id);
+        imgele.src = "pic/pullcarrot_left.gif" + '?' + (new Date().getTime());
+        //changeindex(id);
+    } else if (id == "harvest1" || id == "harvest2" || id == "harvest3") {
+        assignindex(id);
+        imgele.src = "pic/pullcarrot_middle.gif" + '?' + (new Date().getTime());
+        //changeindex(id);
+    } else if (id == "harvest4" || id == "harvest5" || id == "harvest6") {
+        assignindex(id);
+        imgele.src = "pic/pullcarrot_right.gif" + '?' + (new Date().getTime());
+        //changeindex(id);
+    }
+    setTimeout(function () {
+        changeindex(id);
+    }, 3000);
+
 }
 
 function fadeOutPrecount(id) {
@@ -178,43 +224,80 @@ function gameover() {
 
 //答錯
 function answerWrong() {
-    life--;
-    var element = document.getElementById('life');
-    var image = element.querySelectorAll('[src="' + lifeImg + '"]');
-    var wrongclass = document.getElementsByClassName('wrong');
-    wrongclass[0].style.visibility = 'visible';
+    // life--;
+    //var element = document.getElementById('life');
+    //var image = element.querySelectorAll('[src="' + lifeImg + '"]');
     setTimeout(function () {
-        wrongclass[0].style.visibility = 'hidden';
-        if (image != []) {
-            element.removeChild(image[0]);
-        }
-        if (life == 0) {
-            //遊戲結束
-            gameover();
-        } else {
+        var wrongclass = document.getElementsByClassName('wrong');
+        wrongclass[0].style.visibility = 'visible';
+        //叉叉持續2秒
+        setTimeout(function () {
+            wrongclass[0].style.visibility = 'hidden';
+            // if (image != []) {
+            //     element.removeChild(image[0]);
+            // }
+            // if (life == 0) {
+            //     //遊戲結束
+            //     gameover();
+            // } else {
             //這關再來一次
             init();
-        }
-    }, 2000);
+            //}
+        }, 2000);
+    }, 3000)
+
+
 }
 
 //答對
 function answerCorrect() {
-    var correctclass = document.getElementsByClassName('correct');
-    correctclass[0].style.visibility = 'visible';
+    //先讓最後一個拔的動畫完成
     setTimeout(function () {
-        correctclass[0].style.visibility = 'hidden';
-        //進入下一關
-        if (groundnum != 9) {
-            groundnum++;
-            init();
-        } else {
-            //遊戲結束
-            gameover();
-        }
+        var correctclass = document.getElementsByClassName('correct');
+        correctclass[0].style.visibility = 'visible';
+        //打鉤持續2秒
+        setTimeout(function () {
+            correctclass[0].style.visibility = 'hidden';
+            //進入下一關
+            if (groundnum != 9) {
+                groundnum++;
+                init();
+            } else {
+                //遊戲結束
+                gameover();
+            }
 
 
-    }, 2000);
+        }, 2000);
+    }, 3000)
+   
+}
+
+//改變index
+function changeindex(div) {
+    var element = document.getElementById(div);
+
+    if (div.includes("3") || div.includes("6") || div.includes("9")) {
+        element.classList.remove("assignZindex3");
+    } else if (div.includes("2") || div.includes("5") || div.includes("8")) {
+        element.classList.remove("assignZindex2");
+    } else if (div.includes("1") || div.includes("4") || div.includes("7")) {
+        element.classList.remove("assignZindex1");
+    }
+    element.classList.add('changeZindexto0');
+
+}
+function assignindex(div) {
+    var element = document.getElementById(div);
+    if (div.includes("3") || div.includes("6") || div.includes("9")) {
+        element.classList.add('assignZindex3');
+    } else if (div.includes("2") || div.includes("5") || div.includes("8")) {
+        element.classList.add('assignZindex2');
+    } else if (div.includes("1") || div.includes("4") || div.includes("7")) {
+        element.classList.add('assignZindex1');
+    }
+
+
 }
 
 
